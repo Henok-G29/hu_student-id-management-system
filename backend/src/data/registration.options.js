786b4cdb038bac26fa39b148123ca617fd33c588
Henok-G29 <henokgenene29@gmail.com>
2026-08-18 12:01:08 +0300

    add collection of program and educational level data file

---
+++ backend/src/data/registration.options.js
@@ -0,0 +1,165 @@
+// PROGRAM OPTIONS
+
+const programOptions = [
+  {
+    name: "Regular",
+    value: "regular",
+  },
+  {
+    name: "Weekend",
+    value: "weekend",
+  },
+  {
+    name: "Extension",
+    value: "extension",
+  },
+  {
+    name: "Distance",
+    value: "distance",
+  },
+];
+
+// EDUCATIONAL LEVEL OPTIONS
+const educationalLevelOptions = [
+  {
+    name: "Degree",
+    value: "degree",
+  },
+  {
+    name: "TVET",
+    value: "tvet",
+  },
+  {
+    name: "Masters Program Regular",
+    value: "masters",
+  },
+  {
+    name: "Distance Degree",
+    value: "distance_degree",
+  },
+];
+
+// DEGREE DEPARTMENTS
+const degreeDepartments = [
+  {
+    name: "Accounting and Finance",
+    value: "degree_accounting_finance",
+  },
+  {
+    name: "Business Administration",
+    value: "degree_business_administration",
+  },
+  {
+    name: "Marketing Management",
+    value: "degree_marketing_management",
+  },
+  {
+    name: "Economics",
+    value: "degree_economics",
+  },
+  {
+    name: "Computer Science",
+    value: "degree_computer_science",
+  },
+  {
+    name: "Nursing",
+    value: "degree_nursing",
+  },
+  {
+    name: "Pharmacy",
+    value: "degree_pharmacy",
+  },
+  {
+    name: "Public Health",
+    value: "degree_public_health",
+  },
+  {
+    name: "Management",
+    value: "degree_management",
+  },
+];
+
+// TVET DEPARTMENTS
+const tvetDepartments = [
+  {
+    name: "Pharmacy",
+    value: "tvet_pharmacy",
+  },
+  {
+    name: "Nursing",
+    value: "tvet_nursing",
+  },
+  {
+    name: "Animal Health (Veterinary)",
+    value: "tvet_animal_health",
+  },
+  {
+    name: "Surveying Technology",
+    value: "tvet_surveying",
+  },
+  {
+    name: "Building Electrical Installation",
+    value: "tvet_building_electrical",
+  },
+  {
+    name: "Tourism and Hotel Management",
+    value: "tvet_tourism_hotel",
+  },
+  {
+    name: "Industrial Electrical & Electronic Technology",
+    value: "tvet_industrial_electrical",
+  },
+  {
+    name: "Marketing and Sales Management",
+    value: "tvet_marketing_sales",
+  },
+  {
+    name: "IT Hardware and Networking Service",
+    value: "tvet_it_hardware_networking",
+  },
+  {
+    name: "IT Web Development and Database Administration",
+    value: "tvet_it_web_database",
+  },
+  {
+    name: "Secretarial and Office Administration",
+    value: "tvet_secretarial_office",
+  },
+];
+// MASTERS PROGRAMS
+const mastersPrograms = [
+  {
+    name: "Masters of Business Administration",
+    value: "masters_business_administration",
+  },
+  {
+    name: "Masters of Science in Project Management",
+    value: "masters_project_management",
+  },
+];
+
+// DISTANCE DEGREE
+const distanceDegreeDepartments = [
+  {
+    name: "Accounting and Finance",
+    value: "distance_accounting_finance",
+  },
+  {
+    name: "Management",
+    value: "distance_management",
+  },
+  {
+    name: "Economics",
+    value: "distance_economics",
+  },
+];
+
+module.exports = {
+  programOptions,
+  educationalLevelOptions,
+  degreeDepartments,
+  tvetDepartments,
+  mastersPrograms,
+  distanceDegreeDepartments,
+};
+ 
