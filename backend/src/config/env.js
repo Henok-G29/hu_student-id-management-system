fb95f28c78ba1334df02eb2b156dcfe2c4c5aa15
Henok-G29 <henokgenene29@gmail.com>
2026-08-24 10:43:12 +0300

    edit env.js file

--- backend/src/config/env.js
+++ backend/src/config/env.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
 
 const env = {
-  port: Number(process.env.PORT) || 3000,
+  port: Number(process.env.PORT),
 
   database: {
     host: process.env.DB_HOST,
-    port: Number(process.env.DB_PORT) || 3306,
+    port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     name: process.env.DB_NAME,
