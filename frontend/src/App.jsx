251a1b958bf0c724eb3f44e009e995cfc05c4b07
Henok-G29 <henokgenene29@gmail.com>
2026-09-03 13:19:19 +0300

    finished the entire full frontend features

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,16 @@
 import { BrowserRouter } from "react-router-dom";
 
+import { AuthProvider } from "./contexts/AuthContext";
 import AppRoutes from "./routes/AppRoutes";
 
-function App() {
+const App = () => {
   return (
     <BrowserRouter>
-      <AppRoutes />
+      <AuthProvider>
+        <AppRoutes />
+      </AuthProvider>
     </BrowserRouter>
   );
-}
+};
 
 export default App;
